ed2ebf435e0d2ad518ffc2b2ca56e888b3430b6a
kevinalbert7 <20ko@live.fr>
2021-12-15 23:34:08 +0100

    validations

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const port = 5000
 
-const UsersRoutes = require('./routes/users')
+const usersRoutes = require("./routes/users")
 
 app.use(express.json())
 
-app.use('/users', usersRoutes)
+app.use("/users", usersRoutes)
 
 app.listen(port, () => {
     console.log(`server running at port ${port}`)
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,28 @@ const express = require('express')
 const app = express()
 const { body, validationResult } = require('express-validator')
 
-let heroes = require("../users.json")
+let users = require("../users.json")
+
+app.get('/', (req, res) => {
+    res.json(users)
+  })
+
+app.get('/:slug', (req, res) => {
+    const { slug } = req.params
+    const eachUser = users.find(user => user.slug === slug)
+    res.json(eachUser)
+})
+
+app.post('/',   
+    body('name')
+    .exists().withMessage("name is required")
+    .isLength({ min: 4 }).withMessage("name is too short"),
+    body('passwor')
+    .exists().withMessage("passwor is required")
+    .isLength({ min: 8 }).withMessage("password is too short"),
+    body('city')
+    .exists().withMessage("city is required")
+    .isIn(["Paris", "Tokyo", "Los Angeles"]).withMessage("city value is not accepted"),
 
 
 module.exports = app
